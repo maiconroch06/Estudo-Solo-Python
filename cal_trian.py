f10fa0d0aa41a4cea6d21d4665903241b71f7f93
Maicon_Rocha <maiconpablo588@gmail.com>
2025-08-22 00:39:18 -0300

    movendo para outras pastas

--- cal_trian.py
+++
@@ -1,6 +0,0 @@
-# A) Calcular a área de um triângulo a partir de sua base e altura.
-print("Calcule a área do triâgulo")
-base = float(input("Digite a base do triângulo: "))
-altura = float(input("Digite a altura do triângulo: "))
-
-print(f"A área do triangulo é: {(base * altura)/2}")
