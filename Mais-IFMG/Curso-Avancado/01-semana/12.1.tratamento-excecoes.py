9bedbc17f88da24b6bed4b8993f1b9567914c58b
Maicon_Rocha <maiconpablo588@gmail.com>
2025-08-22 22:27:08 -0300

    Create 12.1.tratamento-excecoes.py

---
+++ Mais-IFMG/Curso-Avancado/01-semana/12.1.tratamento-excecoes.py
@@ -0,0 +1,8 @@
+try:
+    print('Informe dois números')
+    n1 = float(input('n1: '))
+    n2 = float(input('n2: '))
+    r = n1 / n2
+    print(n1, '/', n2, '=', r)
+except:
+    print(' # ERRO: erro ocorrido.')
