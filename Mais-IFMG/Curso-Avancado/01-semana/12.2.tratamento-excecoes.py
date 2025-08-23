502a0bc5b44fdad04681178d56c84d44d5955356
Maicon_Rocha <maiconpablo588@gmail.com>
2025-08-22 22:27:02 -0300

    Create 12.2.tratamento-excecoes.py

---
+++ Mais-IFMG/Curso-Avancado/01-semana/12.2.tratamento-excecoes.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        print('Informe dois números')
+        n1 = float(input('n1: '))
+        n2 = float(input('n2: '))
+        r = n1 / n2
+        break
+    except ValueError as e:
+        print(e)
+        print('Número inválidos! Tente novamente.')
+    except ZeroDivisionError as e:
+        print(e)
+        print('Divisão por zero! Tente novamente.')
+        print(n1, '/', n2, '=', r)
